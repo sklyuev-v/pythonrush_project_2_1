63fb03dc0a9a8a28832fddb9a890b302215f4f8b
Sklyuev Viktor <sklyevv@yandex.ru>
2025-04-13 18:14:48 +0300

    Code formated by code formatter

--- static/upload/upload.js
+++ static/upload/upload.js
@@ -1,9 +1,11 @@
 let input = document.querySelector(".input");
 let preview = document.querySelector(".preview");
-let loadBtn = document.getElementById("loadBtn")
+let loadBtn = document.getElementById("loadBtn");
 
 input.addEventListener("change", updateImageDisplay);
-loadBtn.addEventListener('click', () => {uploadFile();});
+loadBtn.addEventListener("click", () => {
+  uploadFile();
+});
 loadBtn.disabled = true;
 
 function updateImageDisplay() {
@@ -20,43 +22,41 @@ function updateImageDisplay() {
   } else {
     const previewDivEl = document.createElement("div");
     preview.appendChild(previewDivEl);
-    
+
     let filenamePEl = document.createElement("p");
-    filenamePEl.className = "mb-0"
-    filenamePEl.id = 'filename'
+    filenamePEl.className = "mb-0";
+    filenamePEl.id = "filename";
 
     let filesizePEl = document.createElement("p");
-      
+
     if (validFileType(curFile)) {
-      loadBtn.disabled = false;      
+      loadBtn.disabled = false;
       filenamePEl.textContent = `Имя: ${curFile.name}`;
       filesizePEl.textContent = `Размер: ${returnFileSize(curFile.size)}`;
-        
+
       let imageElement = document.createElement("img");
-      imageElement.id = 'previewImage'
+      imageElement.id = "previewImage";
       imageElement.src = window.URL.createObjectURL(curFile);
       imageElement.style.width = "100%";
       imageElement.style.height = "auto";
-        
-      previewDivEl.appendChild(imageElement);
-      previewDivEl.appendChild(filenamePEl);      
-      previewDivEl.appendChild(filesizePEl);
 
-    } else {    
+      previewDivEl.appendChild(imageElement);
+      previewDivEl.appendChild(filenamePEl);
+      previewDivEl.appendChild(filesizePEl);
+    } else {
       filenamePEl.textContent = `Имя: ${curFile.name}`;
       filesizePEl.textContent = "Неверный тип файла. Выберите другой.";
       previewDivEl.appendChild(filenamePEl);
       previewDivEl.appendChild(filesizePEl);
-      loadBtn.disabled = true;       
+      loadBtn.disabled = true;
     }
   }
 }
 
-
 const fileTypes = ["image/jpeg", "image/jpg", "image/gif", "image/png"];
 
 function validFileType(file) {
-  return fileTypes.includes(file.type)
+  return fileTypes.includes(file.type);
 }
 
 function returnFileSize(number) {
@@ -67,53 +67,53 @@ function returnFileSize(number) {
   } else if (number > 1048576) {
     return (number / 1048576).toFixed(1) + "MB";
   }
-};
-
+}
 
 function uploadFile() {
   const file = input.files[0];
-  if (!file) return alert('Выберите файл для загрузки.');
+  if (!file) return alert("Выберите файл для загрузки.");
 
-  fetch('/api/upload/', {
-    method: 'POST',
+  fetch("/api/upload/", {
+    method: "POST",
     headers: {
-      'Filename': file.name
+      Filename: file.name,
     },
-    body: file
+    body: file,
   })
-  .then(response => {
-    loadBtn.textContent = 'Успешно загружено'
-    loadBtn.disabled = true
+    .then((response) => {
+      loadBtn.textContent = "Успешно загружено";
+      loadBtn.disabled = true;
 
-    file_location = response.headers.get('Location');
-    filename = response.headers.get('Filename');
+      file_location = response.headers.get("Location");
+      filename = response.headers.get("Filename");
 
-    document.getElementById('filename').textContent = filename;
+      document.getElementById("filename").textContent = filename;
 
-    downloadButton = document.getElementById('downBtn')
-    downloadButton.hidden = false;
-    downloadButton.onclick = () => {
-      const link = document.createElement('a');
-      link.href = file_location + filename;
-      link.download = filename;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+      downloadButton = document.getElementById("downBtn");
+      downloadButton.hidden = false;
+      downloadButton.onclick = () => {
+        const link = document.createElement("a");
+        link.href = file_location + filename;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
 
-    copyLinkButton = document.getElementById('copyBtn')
-    copyLinkButton.hidden = false;
-    copyLinkButton.onclick = () => {
-      navigator.clipboard.writeText(file_location + filename)
-      .then(() => {
-          alert('Ссылка скопирована в буфер обмена!');
-      })
-      .catch(err => {
-          alert('Ошибка при копировании ссылки: ' + err);
-      });
-    }
-  })
-  .catch(error => {
-    console.error('Ошибка загрузки:', error);
-  });
+      copyLinkButton = document.getElementById("copyBtn");
+      copyLinkButton.hidden = false;
+      copyLinkButton.onclick = () => {
+        navigator.clipboard
+          .writeText(file_location + filename)
+          .then(() => {
+            alert("Ссылка скопирована в буфер обмена!");
+          })
+          .catch((err) => {
+            alert("Ошибка при копировании ссылки: " + err);
+          });
+      };
+    })
+    .catch((error) => {
+      console.error("Ошибка загрузки:", error);
+    });
 }
