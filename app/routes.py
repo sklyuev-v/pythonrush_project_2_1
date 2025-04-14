52c3ca8a58e73b0e28151b53766d02bdbba50234
Sklyuev Viktor <sklyevv@yandex.ru>
2025-04-14 20:51:18 +0300

    Add routes for api/delete and api/images-list

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,8 @@ from AdvancedHandler import AdvancedHTTPRequestHandler
 
 
 def register_routes(router: Router, handler_class: AdvancedHTTPRequestHandler):
-    router.add_route('GET', '/api/images/', handler_class.get_images)
+    router.add_route('GET', '/api/images/', handler_class.get_image_gallery)
+    router.add_route('GET', '/api/images-list/', handler_class.get_image_list)
     router.add_route('POST', '/upload/', handler_class.post_upload)
+    router.add_route('DELETE', '/api/delete/<image_id>',
+                     handler_class.delete_image)
